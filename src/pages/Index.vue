ab5d575007d7921fdf1676a0663696dce371a32b
Deep Research <fisheve@mx.lakeforest.edu>
2021-10-25 21:07:43 -0500

    Link to Older Versions

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,16 +1,19 @@
 <template>
   <Layout>
 
-    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image alt="Example image" src="~/favicon.png" width="135" />
-
-    <h1>Hello, world!</h1>
-
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-    </p>
-
-    <g-link to="/sample-page">Markdown Page</g-link>
+    <h2>Old Versions of this website</h2>
+    <ul>
+      <li>
+        <a href="https://strapi.victorslibrary.com/">
+          Strapi Version (Stable)
+        </a>
+      </li>
+      <li>
+        <a href="https://nuxt.victorslibrary.com/">
+          Nuxt Version (Broken)
+        </a>
+      </li>
+    </ul>
 
     <h2>Sample Pages</h2>
     <div v-for="edge in $page.posts.edges" :key="edge.node.id">
