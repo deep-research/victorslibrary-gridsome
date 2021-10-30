4034d49831f2b56a6b7a03574707c9ac732faa52
Deep Research <fisheve@mx.lakeforest.edu>
2021-10-30 01:25:26 -0500

    Check Before Displaying Fields

--- src/components/ArticleHeader.vue
+++ src/components/ArticleHeader.vue
@@ -1,8 +1,14 @@
 <template>
   <div>
-    <div v-if="articleData.authors">By: {{ articleData.authors }}</div>
-    <div v-if="articleData.published">Published: {{ articleData.published }}</div>
-    <div v-if="articleData.updated">Updated: {{ articleData.updated }}</div>
+    <div v-if="articleData.authors">
+      By: {{articleData.authors.join(', ')}}
+    </div>
+    <div v-if="articleData.published">
+      Published: {{ articleData.published }}
+    </div>
+    <div v-if="articleData.updated">
+      Updated: {{ articleData.updated }}
+    </div>
   </div>
 </template>
 
--- src/components/ReenchantmentSong.vue
+++ src/components/ReenchantmentSong.vue
@@ -1,16 +1,35 @@
 <template>
   <div>
-    <div>By: {{ songData.band }} ({{ songData.authors }})</div>  
-    <div>Explicit: {{songData.explicit}}</div>
+    <div v-if="songData.band">
+      By: {{ songData.band }}
+      <span v-if="songData.authors">
+        ({{ songData.authors.join(', ') }})
+      </span>
+    </div>
+    <div v-else>
+      <span v-if="songData.authors">
+        By: {{ songData.authors.join(', ') }}
+      </span>     
+    </div>
 
-    <h2>Description</h2>
-    <div>{{ songData.description }}</div>
+    <div v-if="songData.explicit">
+      Explicit: {{songData.explicit}}
+    </div>
 
-    <h2>Recordings</h2>
-    <div>{{ songData.recordings }}</div>
+    <div v-if="songData.description">
+      <h2>Description</h2>
+      <div>{{ songData.description }}</div>
+    </div>
 
-    <h2>Lyrics</h2>
-    <div>{{ songData.lyrics }}</div>
+    <div v-if="songData.recordings">
+      <h2>Recordings</h2>
+      <div>{{ songData.recordings }}</div>
+    </div>
+
+    <div v-if="songData.lyrics">
+      <h2>Lyrics</h2>
+      <div>{{ songData.lyrics }}</div>
+    </div>
   </div>
 </template>
 
