04bab99a928f29a5ca9b6cc031c694f7ca40492a
Deep Research <fisheve@mx.lakeforest.edu>
2021-10-30 07:09:28 -0500

    Date Mixin Works on Articles

--- src/components/ArticleHeader.vue
+++ src/components/ArticleHeader.vue
@@ -4,21 +4,24 @@
       By: {{articleData.authors.join(', ')}}
     </div>
     <div v-if="articleData.published">
-      Published: {{ articleData.published }}
+      Published: {{ formatDate(articleData.published) }}
     </div>
     <div v-if="articleData.updated">
-      Updated: {{ articleData.updated }}
+      Updated: {{ formatDate(articleData.updated) }}
     </div>
   </div>
 </template>
 
 <script>
+import dateMixin from "../mixins/dateMixin.js"
+
 export default{
   props: {
     articleData: {
       type: Object,
       required: true
     }
-  }
+  },
+  mixins: [dateMixin]
 }
 </script>
