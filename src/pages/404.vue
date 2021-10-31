69948d1be8bf20b565b2acb5462a0c5e619e1bb5
Deep Research <fisheve@mx.lakeforest.edu>
2021-10-31 01:52:14 -0500

    404 Page Styled

---
+++ src/pages/404.vue
@@ -0,0 +1,14 @@
+<template>
+  <Layout>
+    <h2>404 - Page Not Found</h2>
+    <g-link to="/">Home Page</g-link>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: '404 - Page Not Found'
+  }
+}
+</script>
