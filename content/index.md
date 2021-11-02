f4afef9baffab8d056e42f0b347e8bf0f008e209
Deep Research <fisheve@mx.lakeforest.edu>
2021-11-02 01:46:02 -0500

    Everything on home page

--- content/index.md
+++ content/index.md
@@ -4,4 +4,6 @@ authors: ["Victor Fisher"]
 updated: "2021-10-27"
 ---
 
-<g-link to="/versions">Versions</g-link>
+import search from '~/components/search.vue'
+
+<search />
---
+++ src/components/search.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <!-- Results Count -->
+    Results: {{filteredData.length}}
+
+    <!-- Sorted Results -->
+    <ol>
+      <li v-for="node in filteredData" :key="node.id">
+        <g-link :to="node.path">{{ node.title }}</g-link>
+      </li>
+    </ol>
+  </div>
+</template>
+
+<static-query>
+query {
+  pages: allContent {
+    edges {
+      node {
+        id
+        title
+        path
+      }
+    }
+  }
+}
+</static-query>
+
+<script>
+export default{
+  props: {
+    filter: {
+      type: String,
+      required: false
+    }
+  },
+  data() {
+    return {
+      filteredData: []
+    }
+  },
+  methods: {
+    compare(a, b) {
+      let x = a.title.toLowerCase()
+      let y = b.title.toLowerCase()
+      if ( x < y ){
+        return -1;
+      }
+      if ( x > y ){
+        return 1;
+      }
+      return 0;
+    }
+  },
+  mounted() {
+    for (let item of this.$static.pages.edges) {
+       this.filteredData.push(item.node)
+    }
+    // Sort the Data
+    this.filteredData.sort(this.compare);
+  }
+}
+</script>
